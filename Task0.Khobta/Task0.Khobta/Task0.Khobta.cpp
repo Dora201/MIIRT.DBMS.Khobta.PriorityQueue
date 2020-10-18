ec48fd515ef10f7afa4765464009728422ec96c1
Дарья Хобта <daryakhobta@yandex.ru>
2020-10-18 16:18:44 +0300

    Исправила нулевое задание. Добавила srand() и исправила output()

---
+++ Task0.Khobta/Task0.Khobta/Task0.Khobta.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <tchar.h>
+#include <conio.h>
+#include <math.h>
+#include <time.h>
+using namespace std;
+
+
+bool input(const size_t size)
+{
+	return size > 0;
+}
+
+void create(int* value, const size_t size)
+{
+
+	for (int i = 0; i < size; i++)
+	{
+		value[i] = rand();
+	}
+}
+
+void output(const int* value, const size_t size)
+{
+
+	for (int i = 0; i < size - 1; i++)
+	{
+		cout << value[i] << ",";
+	}
+	cout << value[size - 1] << endl;
+
+}
+
+int sum(const int* value, const size_t size)
+{
+	int mysum = 0;
+	for (int i = 0; i < size; i++)
+	{
+		mysum += value[i];
+	}
+	return mysum;
+}
+
+double average(double sum, const size_t size)
+{
+	return sum / size;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	srand(time(NULL));
+	size_t size;
+	cout << "Введите размер массива" << endl;
+	cin >> size;
+
+	if (input(size))
+	{
+		const auto value = new int[size];
+		create(value, size);
+		cout << "Массив: ";
+		output(value, size);
+		cout << "Среднее арифметическое значение = " << average(sum(value, size), size) << endl;
+
+	}
+	else
+	{
+		cout << "Неверный размер массива" << endl;
+	}
+
+}
